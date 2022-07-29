e55b0bf9f302ae49f25d9582ef3fc8ecd871506e
Lab Computo <Computosala42022@ugb.edu.sv>
2022-07-29 09:50:49 -0600

    segundo programa

---
+++ ale 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ale_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string firstname = "rafael";
+            String lastname = "alessandro";
+            int edad = 18;
+            var name = firstname + " " + lastname;
+
+            Console.WriteLine("Su nombre es: {1} y su edad es:{0} ", edad,name);
+        }
+    }
+}
--- practica ale/Program.cs
+++ practica ale/Program.cs
@@ -6,7 +6,17 @@ namespace practica_ale
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = 5;
+            int num2 = 4;
+            var total = num1 + num2;
+           
+            
+            String name = "ale";
+            int edad =  18;
+
+            Console.WriteLine("la suma es:"+ total);
+            Console.WriteLine(name+ " " + edad);
         }
+
     }
 }
