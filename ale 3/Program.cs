8e852e9fb15e57d69748bacfb1ad420f0e7e9a26
Rafa Lopez <alexander20031127@outlook.com>
2022-08-02 00:58:37 -0600

    tarea

--- ale 3/Program.cs
+++ ale 3/Program.cs
@@ -11,9 +11,9 @@ namespace ale_3
             int num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("ingrese otro numero");
             int num2 = int.Parse(Console.ReadLine());
-            var suma = num1 + num2;
+            var resta = num1 + num2;
 
-            Console.WriteLine("la suma de los numeros es : {0}", suma);
+            Console.WriteLine("la resta de los numeros es : {0}", resta);
         }
     }   
 }
--- ale 4/Program.cs
+++ ale 4/Program.cs
@@ -11,10 +11,10 @@ namespace ale_4
             int num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("ingrese otro numero");
             int num2 = int.Parse(Console.ReadLine());
-            var resta = num1 - num2;
+            var suma = num1 + num2;
 
-            Console.WriteLine("la suma de los numeros es : {0}", resta);
+            Console.WriteLine("la suma de los numeros es : {0}", suma);
         }
-    }
+    }   
 }
-
+}
